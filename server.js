4a794d31481df971b666971d5e0343e35bffdac1
Iskandar <93510581+Iskandar006@users.noreply.github.com>
2025-02-01 21:15:32 +0300

    server.js

--- server.js
+++ server.js
@@ -8,34 +8,29 @@ app.use(cors());
 
 const FILE = "products.json";
 
-// Agar fayl mavjud bo‘lmasa, uni yaratish
-if (!fs.existsSync(FILE)) {
-  fs.writeFileSync(FILE, "[]", "utf8");
-}
-
 // Barcha mahsulotlarni olish
 app.get("/api", (req, res) => {
-  const data = JSON.parse(fs.readFileSync(FILE, "utf8"));
+  const data = JSON.parse(fs.readFileSync(FILE));
   res.json(data);
 });
 
 // Yangi mahsulot qo‘shish
 app.post("/api", (req, res) => {
-  const data = JSON.parse(fs.readFileSync(FILE, "utf8"));
+  const data = JSON.parse(fs.readFileSync(FILE));
   const newProduct = {
     id: data.length ? data[data.length - 1].id + 1 : 1,
     ...req.body,
   };
   data.push(newProduct);
-  fs.writeFileSync(FILE, JSON.stringify(data, null, 2), "utf8");
+  fs.writeFileSync(FILE, JSON.stringify(data, null, 2));
   res.json(newProduct);
 });
 
 // Mahsulotni o‘chirish
 app.delete("/api/:id", (req, res) => {
-  let data = JSON.parse(fs.readFileSync(FILE, "utf8"));
+  let data = JSON.parse(fs.readFileSync(FILE));
   data = data.filter((product) => product.id !== parseInt(req.params.id));
-  fs.writeFileSync(FILE, JSON.stringify(data, null, 2), "utf8");
+  fs.writeFileSync(FILE, JSON.stringify(data, null, 2));
   res.json({ message: "O‘chirildi!" });
 });
 
